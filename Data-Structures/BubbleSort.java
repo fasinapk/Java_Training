27259109d4f1911aea31289f6fb8a5abbbfdb708
Fasina P K <69600239+fasinapk@users.noreply.github.com>
2022-02-20 13:21:46 +0530

    Add files via upload

---
+++ Data-Structures/BubbleSort.java
@@ -0,0 +1,45 @@
+package DataStructures;
+
+public class BubbleSort {
+	
+	void bubbleSort(int arr[])
+	{
+		int n=arr.length;
+		for(int i=0;i<n-1;i++)
+			for(int j=0;j<n-1-1;j++)
+			{
+				if(arr[j]>arr[j+1])
+				{
+					//swap arr[j+1] and arr[j]
+					int temp=arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+				}
+				//for debugging
+//				for(int k=0;k<n;++k)
+//					System.out.print(arr[k] + " ");
+//				System.out.println();
+			}
+		
+		
+	}
+	
+   //print array
+	void printArray(int arr[])
+	{
+		int n=arr.length;
+		for(int i=0;i<n;++i)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+	public static void main(String[] args) {
+		BubbleSort ob=new BubbleSort();
+		int arr[] = {64,34,25,12,22,11,90};
+		ob.bubbleSort(arr);
+		System.out.println("sorted array");
+		ob.printArray(arr);
+		
+
+	}
+
+}
